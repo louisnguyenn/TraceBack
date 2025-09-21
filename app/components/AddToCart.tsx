7ed46421b9f5d8661038d621b6a284453d5b7925
Louis Nguyen <louis.nguyen550@gmail.com>
2025-09-20 22:05:51 -0400

    add page components

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+'use client'; // tells the next.js complier to run in client side
+import React from 'react';
+
+const AddToCart = () => {
+	return (
+		<div>
+			<button onClick={() => console.log('Click')}></button>
+		</div>
+	);
+};
+
+export default AddToCart;
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,16 @@
+//
+// this component will be rendered in server side
+//
+
+import React from 'react';
+import AddToCart from "./AddToCart";
+
+const ProductCard = () => {
+	return (
+		<div>
+      <AddToCart />
+		</div>
+	);
+};
+
+export default ProductCard;
